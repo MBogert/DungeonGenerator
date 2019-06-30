1dafb4a285bafbfdbee99f5d5cc472451bf348fa
Matthew Bogert <mabogert64@gmail.com>
2019-06-29 21:15:53 -0700

    Starting Commit

---
+++ Scripts/DungeonGenerator.ts
@@ -0,0 +1,83 @@
+import { DungeonParms } from "./DungeonParms";
+import { RoomGenerator } from "./RoomGenerator";
+import { GapGenerator } from "./GapGenerator";
+import { EncounterGenerator } from "./EncounterGenerator";
+
+class DungeonGenerator extends RoomGenerator{
+	private _parameters: DungeonParms;
+
+	constructor(params: DungeonParms){
+		super();
+		this._parameters = params;
+	}
+
+	public get parameters(): DungeonParms{ return this._parameters;}
+
+	public generateDungeon(): string[][] {
+
+		if(this.parameters.arena == true){
+			return this.buildArena(this.parameters);
+		}else{
+			return this.buildDungeon(this.parameters)
+		}
+	}
+
+	private buildDungeon(parameters: DungeonParms): string[][] {
+
+		let initialDungeon: string[][] = this.buildEmptyDungeon(parameters.length, parameters.width);
+		let roomDungeon: string[][] = this.populateDungeonRooms(initialDungeon);
+		let connectedDungeon: string[][] = this.connectRooms(roomDungeon);
+		let gapDungeon: string[][] = this.populateGaps(connectedDungeon, parameters.gapDensity);
+		let populateEncounters: string[][] = this.populateEncounters(gapDungeon, parameters.encounterDensity);
+
+		return populateEncounters;
+	}
+
+	private buildArena(parameters: DungeonParms): string[][] {
+
+		let initialDungeon: string[][] = this.buildEmptyDungeon(parameters.length, parameters.width);
+		let roomDungeon: string[][] = this.populateArenaRoom(initialDungeon);
+		let gapDungeon: string[][] = this.populateGaps(roomDungeon, parameters.gapDensity);
+		let populateEncounters: string[][] = this.populateEncounters(gapDungeon, parameters.encounterDensity);
+
+		return populateEncounters;
+	}
+
+	private buildEmptyDungeon(length: number, width: number): string[][] {
+		
+		let newDungeon: string[][];
+		for(let i = 0; i < length; i ++){
+			newDungeon[i] = [];
+			for(let j = 0; j < length; j++){
+				newDungeon[i][j] = "x";
+			}
+		}
+		return newDungeon;
+	}
+
+	private populateDungeonRooms(dungeon: string[][]): string[][] {
+
+		return null;
+	}
+
+	private populateArenaRoom(dungeon: string[][]): string[][] {
+
+		return null;
+	}
+
+	private connectRooms(dungeon: string[][]): string[][] {
+
+		return null;
+	}
+
+	private populateGaps(dungeon: string[][], gapDensity: number): string[][] {
+
+		return null;
+	}
+
+	private populateEncounters(duneon: string[][], encounterDensity: number) {
+
+		return null;
+	}
+
+}
---
+++ Scripts/DungeonParms.ts
@@ -0,0 +1,28 @@
+export class DungeonParms {
+
+	private _length: number;
+	private _width: number;
+	private _arena: boolean;
+	private _type: string; // May eventually become its own class ClassName extends AnotherClass {
+	private _encounterDensity: number;
+	private _gapDensity: number;
+
+	constructor(length: number, width: number, arena: boolean, encounterDensity: number, gapDensity: number) {
+		this._length = length;
+		this._width = width;
+		this._arena = arena;
+		this._gapDensity = gapDensity;
+		this._encounterDensity = encounterDensity;
+	}
+
+	// Getters
+	public get length(): number {return this._length;}
+	public get width(): number {return this._width;}
+	public get arena(): boolean {return this._arena;}
+	public get type(): string {return this._type}
+	public get encounterDensity(): number {return this._encounterDensity}
+	public get gapDensity(): number {return this._gapDensity;}
+
+	// Setters (none atm for current needs)
+
+}
