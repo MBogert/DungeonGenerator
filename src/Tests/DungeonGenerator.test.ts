c58ee32963e4e3abe43337d3df19b9b09ef67a08
Matthew Bogert <mabogert64@gmail.com>
2019-07-14 13:00:51 -0700

    First full UTs for Room Class

--- src/Tests/DungeonGenerator.test.ts
+++ src/Tests/DungeonGenerator.test.ts
@@ -1,15 +1,26 @@
 import { DungeonGenerator } from '../Scripts/DungeonGenerator';
 import { DungeonParms } from '../Scripts/DungeonParms';
+import { Room } from '../Scripts/Room';
 
-test('Will this work?', () => {
-	expect(Math.max(1,2)).toBe(2);
+// test('Will this work?', () => {
+// 	expect(Math.max(1,2)).toBe(2);
+// });
+
+// test('DummyDungeon', () => {
+
+// 	let parms: DungeonParms = new DungeonParms(10, 10, false, 0, 0);
+// 	let generator: DungeonGenerator = new DungeonGenerator(parms);
+// 	let dungeon: string[][] = generator.generateDungeon();
+
+
+// });
+
+test('RoomLength', () => {
+	let room: Room = new Room(7, 4, 10, 11);
+	expect(room.length).toBe(10);
 });
 
-test('DummyDungeon', () => {
-
-	let parms: DungeonParms = new DungeonParms(10, 10, false, 0, 0);
-	let generator: DungeonGenerator = new DungeonGenerator(parms);
-	let dungeon: string[][] = generator.generateDungeon();
-
-
+test('RoomWidth', () => {
+	let room: Room = new Room(7, 4, 10, 11);
+	expect(room.width).toBe(11);
 });
---
+++ src/Tests/Room.test.ts
@@ -0,0 +1,52 @@
+import { Room } from '../Scripts/Room';
+
+let testRoom: Room;
+beforeAll(() => {
+	testRoom = new Room(2, 4, 7, 9);
+});
+
+afterAll(() => {
+	// Void
+});
+
+
+// Dimensions
+test('Length', () => {
+	expect(testRoom.length).toBe(7);
+});
+
+test('Width', () => {
+	expect(testRoom.width).toBe(9);
+})
+
+
+// Coordinates
+test('West Coordinate', () => {
+	expect(testRoom.westCoordinate).toBe(2);
+});
+
+test('East Coordinate', () => {
+	expect(testRoom.eastCoordinate).toBe(9);
+});
+
+test('South Coordinate', () => {
+	expect(testRoom.southCoordinate).toBe(4);
+});
+
+test('North Coordinate', () => {
+	expect(testRoom.northCoordinate).toBe(13);
+});
+
+
+// Center
+test('Center Tuple', () => {
+	expect(testRoom.center).toStrictEqual([5,8]);
+});
+
+test('Center X-Coordinate', () => {
+	expect(testRoom.centerXCoordinate).toBe(5);
+});
+
+test('Center Y-Coordinate', () => {
+	expect(testRoom.centerYCoordinate).toBe(8);
+});
